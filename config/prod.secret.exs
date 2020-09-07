d642cf891e778ce647da87d39d6cbad492457d88
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-09-08 06:50:29 +0800

    remove ,

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -30,7 +30,7 @@ config :sunstone, SunstoneWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base,
+  secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
 #
