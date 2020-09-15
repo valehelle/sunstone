aeacbf77f2349383b6ac0fbd4d030a4ac906a5ab
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-09-15 17:13:06 +0800

    change email invite

--- lib/sunstone_web/controllers/invite_controller.ex
+++ lib/sunstone_web/controllers/invite_controller.ex
@@ -16,7 +16,7 @@ defmodule SunstoneWeb.InviteController do
   def create(conn, %{"invite" => %{"email" => email} = invite,"hash_id" => hash_id})  do
     office_id = SunstoneWeb.UserController.decode_id(hash_id)
     office = Chats.get_office!(office_id)
-    emails = String.split(email,",");
+    emails = String.replace(email, " ", "") |> String.split(",");
     Enum.each emails, fn(email) ->
       case Chats.get_invite_from_email!(email, office) do
         nil ->
