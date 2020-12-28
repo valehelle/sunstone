1abc402beb67409007105c5ee9ec9606bc81216a
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-12-28 16:31:50 +0800

    change it again

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :sunstone, SunstoneWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "www.getfluorite.com", port: 443],
+  url: [scheme: "https", host: "www.inoffice.chat", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
